ac9c915e8dc2966bdf3358c4177d30964df9d85c
gab-braga <gabrielbraga0712@gmail.com>
2024-10-26 14:59:01 -0300

    feat: logo cubos component added

--- src/components/icons/IconChevronDown.jsx
+++ src/components/icons/IconChevronDown.jsx
@@ -1,5 +1,5 @@
 export default () => {
-    const classStyle = "fill-current stroke-current text-purple-975 dark:text-purple-dark-975";
+    const classStyle = "fill-current stroke-current text-mauve-975 dark:text-mauve-dark-975";
     return (
         <svg className={classStyle} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M18 9L12 15L6 9" strokeWidth="2" fill="none" />
