e2e36eeacbe601542b4dccd25f444942e4a688d8
gab-braga <gabrielbraga0712@gmail.com>
2024-10-27 23:42:58 -0300

    feat: field for filter added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import api from "../../api/config";
 import MovieCard from "../../components/movie-card";
 import Pagination from "../../components/pagination";
+import IconSearch from "../../components/icons/IconSearch";
 
 async function searchGenres() {
     const { data } = await api.get("/genre/movie/list", {
@@ -47,13 +48,13 @@ export default () => {
     }
 
     function handlePreviousPage() {
-        const previous = page-1;
-        if (previous >= 1) setPage(value => value-1);
+        const previous = page - 1;
+        if (previous >= 1) setPage(value => value - 1);
     }
 
     function handleNextPage() {
-        const next = page+1;
-        if (next <= 500) setPage(value => value+1);
+        const next = page + 1;
+        if (next <= 500) setPage(value => value + 1);
     }
 
     function handleChangePage(newPage) {
@@ -66,10 +67,18 @@ export default () => {
 
     return (
         <main className="w-full flex-1 p-8">
+            <div className="w-full flex justify-center items-center pb-6">
+                <div className="flex-1 flex justify-center items-center max-w-[488px]">
+                    <input type="text" className="w-full h-14 p-4 bg-mauve-100 dark:bg-mauve-dark-100 border-t border-b border-l border-mauve-600 dark:border-mauve-dark-600 rounded-s outline-none text-mauve-dark-100 dark:text-white" placeholder="Pesquise por filmes" />
+                    <button className="w-14 h-14 bg-mauve-100 dark:bg-mauve-dark-100 border-t border-b border-r border-mauve-600 dark:border-mauve-dark-600 flex justify-center items-center rounded-e outline-none">
+                        <IconSearch />
+                    </button>
+                </div>
+            </div>
             <div className="bg-[#1414153b] dark:bg-[#EBEAF814] p-6 rounded backdrop-blur-sm grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                 {movies.map((movie, idx) => <MovieCard {...movie} key={idx} />)}
             </div>
-            <Pagination {...{page, handlePreviousPage, handleNextPage, handleChangePage}} />
+            <Pagination {...{ page, handlePreviousPage, handleNextPage, handleChangePage }} />
         </main>
     );
 }
