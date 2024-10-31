1ea189077f18f76e98e947476f3c2aa26d9fb18e
gab-braga <gabrielbraga0712@gmail.com>
2024-10-30 23:21:21 -0300

    style: style of movie page updated

--- src/pages/movie/index.jsx
+++ src/pages/movie/index.jsx
@@ -6,6 +6,7 @@ import api, { getUrlPoster } from "../../api/config";
 import ProgressCircle from "../../components/progress-circle";
 import { formatDate, formatHour } from "../../helps/date";
 import { abbreviateMoney } from "../../helps/number";
+import StandardPoster from "../../assets/movie.png";
 
 async function searchMovie(id) {
     const { data } = await api.get(`/movie/${id}`, {
@@ -35,6 +36,7 @@ export default () => {
     const [movie, setMovie] = useState({});
     const [video, setVideo] = useState({});
     const [movieGenres, setMovieGenres] = useState([]);
+    const [poster, setPoster] = useState(StandardPoster);
     const { id } = useParams();
 
     function getMovieGenres(movie) {
@@ -57,85 +59,91 @@ export default () => {
         loadData();
     }, [id]);
 
+    useEffect(() => {
+        if (movie.poster_path)
+            setPoster(getUrlPoster(movie.poster_path));
+        else setPoster(StandardPoster);
+    }, [movie]);
+
     return (
-        <main className="w-full flex-1 p-8">
+        <main className="w-full flex-1 p-4 xs:p-8">
             <div
-                style={{ backgroundImage: `url(${getUrlPoster(movie.backdrop_path)})` }}
-                className="w-full bg-cover" >
-                <div className="w-full h-full bg-movie-gradient-dark p-8 flex gap-8">
-                    <div className="max-w-72">
-                        <img src={getUrlPoster(movie.poster_path)} className="w-full rounded" />
+                style={{ backgroundImage: `url(${getUrlPoster(movie?.backdrop_path)})` }}
+                className="movie-info w-full bg-none bg-cover" >
+                <div className="w-full h-full bg-movie-gradient-dark sm:p8 flex flex-col items-center lg:flex-row gap-8">
+                    <div className="flex-1 min-w-64 max-w-80">
+                        <img src={poster} className="w-full rounded" />
                     </div>
                     <div className="flex-1 flex flex-col gap-4">
-                        <div className="flex justify-between items-center">
-                            <div className="flex flex-col justify-between gap-2">
+                        <div className="flex flex-wrap gap-8 justify-between items-center">
+                            <div className="flex flex-col justify-between gap-2 min-w-60 sm:min-w-80">
                                 <div className="flex flex-col">
-                                    <h1 className="text-3xl text-mauve-dark-975 font-semibold">{movie.title}</h1>
-                                    <span className="text-mauve-950">Título original: {movie.original_title}</span>
+                                    <h1 className="text-3xl text-mauve-dark-975 font-semibold">{movie?.title}</h1>
+                                    <span className="text-mauve-950">Título original: {movie?.original_title}</span>
                                 </div>
-                                <span className="text-mauve-dark-975 italic">{movie.tagline}</span>
+                                <span className="text-mauve-dark-975 italic">{movie?.tagline}</span>
                             </div>
-                            <div className="flex gap-4 items-center">
+                            <div className="flex flex-wrap gap-4 items-center min-w-60 sm:min-w-80">
                                 <InfoFlash title="Popularidade">
                                     <span className="text-mauve-dark-975 text-sm font-bold">
-                                        {movie.popularity}
+                                        {movie?.popularity}
                                     </span>
                                 </InfoFlash>
                                 <InfoFlash title="Votos">
                                     <span className="text-mauve-dark-975 text-sm font-bold">
-                                        {movie.vote_count}
+                                        {movie?.vote_count}
                                     </span>
                                 </InfoFlash>
-                                <ProgressCircle progress={parseInt(movie.vote_average * 10)} size={140} />
+                                <ProgressCircle progress={parseInt(movie?.vote_average * 10)} size={140} />
                             </div>
                         </div>
-                        <div className="flex gap-4">
-                            <div className="flex-1 flex flex-col gap-4">
+                        <div className="flex flex-wrap gap-4">
+                            <div className="flex-1 flex flex-col gap-4 min-w-60 sm:min-w-80">
                                 <InfoFlash title="Sinopse">
                                     <span className="text-mauve-dark-975 font-normal">
-                                        {movie.overview}
+                                        {movie?.overview}
                                     </span>
                                 </InfoFlash>
                                 <InfoFlash title="Gêneros">
-                                    <div className="flex gap-2">
+                                    <div className="flex flex-wrap gap-2">
                                         {movieGenres.map((genre, idx) => (
                                             <span className="block p-2 text-purple-dark-975 text-xs font-semibold uppercase bg-[#C150FF2E] rounded-sm" key={idx}>{genre}</span>
                                         ))}
                                     </div>
                                 </InfoFlash>
                             </div>
-                            <div className="flex-1 flex gap-4 flex-wrap items-start content-start">
-                                <InfoFlash title="Lançamento" className="flex-1 max-w-36">
+                            <div className="flex-1 flex gap-4 min-w-60 sm:min-w-80 flex-wrap items-start content-start">
+                                <InfoFlash title="Lançamento" className="flex-1 max-w-40">
                                     <span className="text-mauve-dark-975 text-sm font-bold">
                                         {formatDate(movie.release_date)}
                                     </span>
                                 </InfoFlash>
-                                <InfoFlash title="Duração" className="flex-1 max-w-36">
+                                <InfoFlash title="Duração" className="flex-1 max-w-40">
                                     <span className="text-mauve-dark-975 text-sm font-bold">
                                         {formatHour(movie.runtime)}
                                     </span>
                                 </InfoFlash>
-                                <InfoFlash title="Situação" className="flex-1 max-w-36">
+                                <InfoFlash title="Situação" className="flex-1 max-w-40">
                                     <span className="text-mauve-dark-975 text-sm font-bold">
                                         {movie.status}
                                     </span>
                                 </InfoFlash>
-                                <InfoFlash title="Idioma" className="flex-1 max-w-36">
+                                <InfoFlash title="Idioma" className="flex-1 max-w-40">
                                     <span className="text-mauve-dark-975 text-sm font-bold">
                                         {movie.original_language}
                                     </span>
                                 </InfoFlash>
-                                <InfoFlash title="Orçamento" className="flex-1 max-w-36">
+                                <InfoFlash title="Orçamento" className="flex-1 max-w-40">
                                     <span className="text-mauve-dark-975 text-sm font-bold">
                                         ${abbreviateMoney(movie.budget)}
                                     </span>
                                 </InfoFlash>
-                                <InfoFlash title="Receita" className="flex-1 max-w-36">
+                                <InfoFlash title="Receita" className="flex-1 max-w-40">
                                     <span className="text-mauve-dark-975 text-sm font-bold">
                                         ${abbreviateMoney(movie.revenue)}
                                     </span>
                                 </InfoFlash>
-                                <InfoFlash title="Lucro" className="flex-1 max-w-36">
+                                <InfoFlash title="Lucro" className="flex-1 max-w-40">
                                     <span className="text-mauve-dark-975 text-sm font-bold">
                                         ${abbreviateMoney(movie.revenue - movie.budget)}
                                     </span>
@@ -146,13 +154,19 @@ export default () => {
                 </div>
             </div>
 
-            <h2 className="text-mauve-dark-975 font-bold mt-8 mb-4">Trailer</h2>
+            {video && (
+                <>
+                    <h2 className="text-mauve-dark-975 font-bold mt-8 mb-4">Trailer</h2>
 
-            <iframe width="560" height="315" title={video.name}
-                className="w-full h-[600px]"
-                src={`https://www.youtube.com/embed/${video.key}`}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin" frameBorder="0" allowFullScreen></iframe>
+                    <div className="relative w-full pb-[56.25%] overflow-hidden">
+                        <iframe width="560" height="315" title={video?.name}
+                            className="absolute top-0 left-0 w-full h-full"
+                            src={`https://www.youtube.com/embed/${video?.key}`}
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerPolicy="strict-origin-when-cross-origin" frameBorder="0" allowFullScreen></iframe>
+                    </div>
+                </>
+            )}
         </main>
     );
 }
